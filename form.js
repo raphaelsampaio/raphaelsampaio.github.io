38f5344aed2bb933c9e38b43bec1f4c11dcadc03
Raphael Sampaio <raphaelsampaio@gmail.com>
2019-02-14 00:55:34 -0200

    bio and styling

---
+++ form.js
@@ -0,0 +1,24 @@
+var formSubmit = function(form) {
+    var url = form.attr('action');
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: form.serialize(),
+        success: function(data) {
+            alert("Obrigado pela mensagem! Redirecionando para a página principal...");
+            window.location.href='index.html';
+        },
+        error: function(e) {
+            alert("Alguma coisa deu errado! Verifique se todos os campos do formulário foram preenchidos");
+            // window.location.href='index.html';
+        }
+    });
+    return false;
+}
+
+$(document).ready(function() {
+    $("#contact-form").submit(function(e) {
+        e.preventDefault();
+        formSubmit($(this));
+    });
+})
